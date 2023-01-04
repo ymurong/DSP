b6732c22a754918f2622aa0383cd1fb6c2583a6a
Yanchao Murong <yanchao.murong@renault.com>
2023-01-04 01:55:29 +0100

    add cors support

--- backend/src/run.py
+++ backend/src/run.py
@@ -2,12 +2,24 @@ from src.transactions import transaction_app
 from fastapi import FastAPI
 import uvicorn
 from fastapi_pagination import add_pagination
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title='Risk API',
     version='1.0.0',
     docs_url='/docs',
     redoc_url='/redoc',
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1",
+        "http://127.0.0.1:8080"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(transaction_app, prefix='/transactions', tags=['transactions'])
