3d36676e83fcfe8bf4a9248ec511b01ee2e7786f
Oriol Aguilarm <aguilars99@gmail.com>
2023-01-23 18:51:02 +0100

    human readable explainability

--- dashboard-front/coreui/src/app/views/dashboard/explainability/explanation/explanation.component.ts
+++ dashboard-front/coreui/src/app/views/dashboard/explainability/explanation/explanation.component.ts
@@ -14,6 +14,9 @@ export class ExplanationComponent implements OnInit{
   @Input() psp_reference: bigint = BigInt(0);
   @Input() risk_score: number = 0;
 
+  public influential_features: string[] = ["email_address_risk_30day_window", "card_nb_tx_30day_window", "email_address_risk_7day_window", "diff_tx_time_in_hours"];
+  public verbose_explanation: string[] = [];
+
   chartRadarData = {
     labels: ['General Evidences Score', 'IP Score', 'Card Behaviour Score', 'Amount Spent Score', 'E-Mail Score'],
     datasets: [
@@ -63,6 +66,39 @@ export class ExplanationComponent implements OnInit{
   fillExplanationScores(explanationScores: any) {
     const data = [explanationScores["general_evidences"], explanationScores["ip_risk"], explanationScores["risk_card_behaviour"], explanationScores["risk_card_amount"], explanationScores["email_risk"]]
     this.chartRadarData.datasets[0].data = data;
+  }
+
+  createSentences(): void {
+    let explanation: string = "";
+    const sentence = this.influential_features[0];
+
+    if (this.risk_score > 0.5) {
+      const token0 = sentence.split("_")[0]
+      if (token0 == "card"){
+        const type = sentence.split("_")[1]
+        if (type == "avg"){
+          const number_days = "7";
+          explanation = "The amount spent with this CARD during the last N days "
+        }else if (type == "nb"){
+          explanation = "The number of transactions made with this CARD during the last N days it's been higher than usual"
+        }
+      } else if (token0 == "email"){
+        const type = sentence.split("_")[2]
+        if (type == "risk"){
+          explanation = "There have been previous transactions made with this EMAIL during the last N days that have been fraudulent."
+        }else if (type == "nb"){
+          explanation = "The number of transactions made with this EMAIL during the last N days it's been higher than usual"
+        }
+      } else if (token0 == "ip"){
+        const type = sentence.split("_")[2]
+        if (type == "risk"){
+          explanation = "There have been previous transactions made with this IP during the last N days that have been fraudulent."
+        }else if (type == "nb"){
+          explanation = "The number of transactions made with this IP during the last N days it's been higher than usual"
+        }
+      }
+      
+    }
   }
 
 
